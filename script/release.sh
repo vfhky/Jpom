9c88417bca1e759b332a967fadebfc34cb05cd49
vfhky <vfhky@qq.com>
2024-04-08 21:24:35 +0800

    脚本.

--- script/release.sh
+++ script/release.sh
@@ -15,9 +15,8 @@
 # 删除前端依赖从新安装
 cd ../ && cd web-vue && rm -rf node_modules
 
-# 构建前端
-# cd ../ && cd web-vue && npm i && npm run build
-cd ../ && cd web-vue && pnpm store prune && pnpm install && pnpm build
+# 构建前端  cd ../ && cd web-vue && pnpm store prune && pnpm install && pnpm build
+cd ../ && cd web-vue && npm i && npm run build
 
 # 构建 Java
 cd ../ && mvn clean && mvn clean package
